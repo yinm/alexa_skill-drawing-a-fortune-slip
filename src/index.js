17a5b9cbfdb728a1aeb608cfe8e8968b164673d9
yinm <yinm@pepabo.com>
2017-07-12 08:15:09 +0900

    必要そうなデータを定義

---
+++ src/index.js
@@ -0,0 +1,23 @@
+'use strict';
+const Alexa = require('alexa-sdk');
+
+
+const SKILL_NAME      = 'Space Facts';
+const DRAWING_MESSAGE = "Here's your fortune: ";
+const HELP_MESSAGE    = 'You can say draw a fortune slip or, you can say exit... What can I help you with?';
+const HELP_REPROMPT   = 'What can I help you with?';
+const STOP_MESSAGE    = 'Goodbye!';
+
+const FORTUNE_SLIPS = [
+  'Excellent luck',    // 大吉
+  'Fair luck',         // 中吉
+  'Uncertain luck',    // 末吉
+  'Bad luck'           // 凶
+];
+
+
+
+
+
+
+
