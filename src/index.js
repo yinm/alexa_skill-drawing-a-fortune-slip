6820e973210e65ad176a2f16795f7641a5f0cea0
yinm <yinm@pepabo.com>
2017-07-12 08:24:14 +0900

    handlerを定義

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 'use strict';
 const Alexa = require('alexa-sdk');
 
-
-const SKILL_NAME      = 'Space Facts';
+const SKILL_NAME      = 'your fortune';
 const DRAWING_MESSAGE = "Here's your fortune: ";
 const HELP_MESSAGE    = 'You can say draw a fortune slip or, you can say exit... What can I help you with?';
 const HELP_REPROMPT   = 'What can I help you with?';
@@ -22,5 +21,26 @@ exports.handler = (event, context, callback) => {
 };
 
 const handlers = {
+  'LaunchRequest': function() {
+    this.emit('GetNewFactIntent');
+  },
+  'DrawAFortuneSlipIntent': function() {
+    const fortuneSlipIndex = Math.floor(Math.random() * FORTUNE_SLIPS.length);
+    const randomFortuneSlip = FORTUNE_SLIPS[fortuneSlipIndex];
+    const speechOutput = DRAWING_MESSAGE + randomFortuneSlip;
 
+    this.emit(':tellWithCard', speechOutput, SKILL_NAME, randomFortuneSlip);
+  },
+  'AMAZON.HelpIntent': function() {
+    const speechOutput = HELP_MESSAGE;
+    const reprompt = HELP_REPROMPT;
+
+    this.emit(':ask', speechOutput, reprompt);
+  },
+  'AMAZON.CancelIntent': function() {
+    this.emit(':tell', STOP_MESSAGE);
+  },
+  'AMAZON.StopIntent': function() {
+    this.emit(':tell', STOP_MESSAGE);
+  }
 };
